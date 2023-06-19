0f4b15a8bee55c7b2b4e66a1d7fadb11aebb1fb8
Rob Whelan <hallwayraptor@gmail.com>
2023-06-19 17:14:18 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <Chicken />;
+}
+
+function Chicken() {
+	const [isChickenTender, setIsChickenTender] = useState(false);
+	const [noButtonClass, setNoButtonClass] = useState("");
+	const [noClicked, setNoClicked] = useState(false);
+
+	const handleYesClick = () => {
+		setIsChickenTender(true);
+		setNoClicked(false);
+	};
+	const handleNoClick = () => {
+		if (
+			noButtonClass === "noButtonHidden" ||
+			noButtonClass === "noButtonDisabled"
+		) {
+			return;
+		}
+		setNoClicked(true);
+	};
+	const handleNoHover = () => {
+		const styles = ["noButtonHidden", "noButtonDisabled"];
+		const randomStyle = styles[Math.floor(Math.random() * styles.length)];
+		setNoButtonClass(randomStyle);
+	};
+
+	const handleNoHoverOut = (event) => {
+		if (event.relatedTarget.className !== "noButtonWrapper") {
+			setNoButtonClass("");
+		}
+	};
+
+	return (
+		<div>
+			<h1>
+				{noClicked
+					? "Don't lie, you do"
+					: isChickenTender
+					? "You're a chicken tender"
+					: "Do you tend to chickens?"}
+			</h1>
+			{!isChickenTender && !noClicked && (
+				<div className="buttonsHolder">
+					<button
+						className={`btnNo ${noButtonClass}`}
+						onMouseEnter={handleNoHover}
+						onMouseOut={handleNoHoverOut}
+						onClick={handleNoClick}
+					>
+						No
+					</button>{" "}
+					<button onClick={handleYesClick} className="btnYes">
+						Yes
+					</button>
+				</div>
+			)}
+			{noClicked && (
+				<button onClick={handleYesClick} className="btnYes">
+					Yes
+				</button>
+			)}
+		</div>
+	);
 }
 
 export default App;
